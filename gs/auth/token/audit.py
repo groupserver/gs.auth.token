b4fbdfc3d476d94b5a55f02dc29fa53b3bdccba8
Michael JasonSmith <mpj17@onlinegroups.net>
2012-09-02 12:05:01 +1000

    A nasty fix for munge_date and a heap of whitespace fixes.

--- gs/auth/token/audit.py
+++ gs/auth/token/audit.py
@@ -4,34 +4,37 @@ from datetime import datetime
 from zope.component.interfaces import IFactory
 from zope.interface import implements, implementedBy
 from Products.GSAuditTrail import IAuditEvent, BasicAuditEvent, \
-    AuditQuery, event_id_from_data
-from createtoken import create_token # A tiny HACK, but it works.
+    AuditQuery
+from Products.XWFCore.XWFUtils import munge_date
+from createtoken import create_token  # A tiny HACK, but it works.
 
 SUBSYSTEM = 'gs.auth.token'
 import logging
-log = logging.getLogger(SUBSYSTEM) #@UndefinedVariable
+log = logging.getLogger(SUBSYSTEM)  #@UndefinedVariable
 
-UNKNOWN         = '0'
-AUTH_FAIL       = '1'
+UNKNOWN   = '0'
+AUTH_FAIL = '1'
+
 
 class AuditEventFactory(object):
     implements(IFactory)
 
-    def __call__(self, context, event_id,  code, date,
-        userInfo, instanceUserInfo,  siteInfo,  groupInfo=None,
+    def __call__(self, context, event_id, code, date,
+        userInfo, instanceUserInfo, siteInfo, groupInfo=None,
         instanceDatum='', supplementaryDatum='', subsystem=''):
         if code == AUTH_FAIL:
-            event = AuthFailEvent(context, event_id, date, siteInfo, 
+            event = AuthFailEvent(context, event_id, date, siteInfo,
                                   instanceDatum)
         else:
-            event = BasicAuditEvent(context, event_id, UNKNOWN, date, 
-                      userInfo, instanceUserInfo, siteInfo, groupInfo, 
+            event = BasicAuditEvent(context, event_id, UNKNOWN, date,
+                      userInfo, instanceUserInfo, siteInfo, groupInfo,
                       instanceDatum, supplementaryDatum, SUBSYSTEM)
         assert event
         return event
-    
+
     def getInterfaces(self):
         return implementedBy(BasicAuditEvent)
+
 
 # AUTH_FAIL       = '1'
 class AuthFailEvent(BasicAuditEvent):
@@ -39,16 +42,16 @@ class AuthFailEvent(BasicAuditEvent):
     implements(IAuditEvent)
 
     def __init__(self, context, id, d, siteInfo, pageId):
-        BasicAuditEvent.__init__(self, context, id,  AUTH_FAIL, d, None, None, 
+        BasicAuditEvent.__init__(self, context, id, AUTH_FAIL, d, None, None,
                                  siteInfo, None, pageId, None, SUBSYSTEM)
-          
+
     def __str__(self):
         retval = u'Authentication failure for the page <%s> on the site %s '\
-            u'(%s).' % (self.instanceDatum, self.siteInfo.name, 
+            u'(%s).' % (self.instanceDatum, self.siteInfo.name,
                         self.siteInfo.id)
         retval = retval.encode('ascii', 'ignore')
         return retval
-    
+
     @property
     def xhtml(self):
         cssClass = u'audit-event gs-group-messages-add-%s' % self.code
@@ -59,6 +62,7 @@ class AuthFailEvent(BasicAuditEvent):
           (retval, munge_date(self.context, self.date))
         return retval
 
+
 class Auditor(object):
     def __init__(self, context, siteInfo):
         self.context = context
@@ -66,14 +70,14 @@ class Auditor(object):
 
         self.queries = AuditQuery()
         self.factory = AuditEventFactory()
-        
-    def info(self, code, instanceDatum= ''):
+
+    def info(self, code, instanceDatum=''):
         d = datetime.now(UTC)
         eventId = create_token()
-          
-        e = self.factory(self.context, eventId,  code, d, 
-                         None,  None, self.siteInfo, None,
+
+        e = self.factory(self.context, eventId, code, d,
+                         None, None, self.siteInfo, None,
                          instanceDatum, None, SUBSYSTEM)
-          
+
         self.queries.store(e)
         log.info(e)
