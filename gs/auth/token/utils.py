32125930322c28b81f6fb50ce629cb359be1686e
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-20 14:58:00 -0700

    Being more flexible if the context and request are AWOL.

--- gs/auth/token/utils.py
+++ gs/auth/token/utils.py
@@ -13,6 +13,7 @@
 #
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
+from functools import reduce
 from operator import or_
 from zope.component import createObject
 from .audit import Auditor, AUTH_FAIL
@@ -20,12 +21,10 @@ from .authtoken import AuthenticationTokenMismatch
 
 
 def log_auth_error(context, request, errors):
-    assert context
-    assert request
     authError = reduce(or_, [isinstance(e[2], AuthenticationTokenMismatch)
                              for e in errors],
                        False)
-    if authError:
+    if authError and context and request:
         siteInfo = createObject('groupserver.SiteInfo', context)
         auditor = Auditor(context, siteInfo)
         auditor.info(AUTH_FAIL, request.URL)
