76345347d0e7ba508662c555d465795c2b5b6889
Michael JasonSmith <mpj17@onlinegroups.net>
2015-06-19 11:04:21 -0700

    For the love of a ,

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sys
 from setuptools import setup, find_packages
 from version import get_version
 
-name = 'gs.auth.token',
+name = 'gs.auth.token'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
